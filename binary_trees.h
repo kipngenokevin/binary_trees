efe5f7bf58bc6a789aa48b53050b457003739218
kipngeno kevin <kipngenokevin254@gmail.com>
2024-01-04 06:41:55 +0000

    function that creates a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -24,5 +24,6 @@ typedef struct binary_tree_s bst_t; /* Binary Search Tree */
 typedef struct binary_tree_s avl_t; /* AVL Tree */
 typedef struct binary_tree_s heap_t; /* Max Binary Heap */
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
